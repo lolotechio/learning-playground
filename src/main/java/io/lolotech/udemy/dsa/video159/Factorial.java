51e8928994e53d0198807d422a5b077cd4d31995
l.olo <lucian.olosutean@arobs.com>
2024-12-10 00:17:46 +0200

    video 159: factorial recursive

---
+++ src/main/java/io/lolotech/udemy/dsa/video159/Factorial.java
@@ -0,0 +1,34 @@
+package io.lolotech.udemy.dsa.video159;
+
+public class Factorial {
+    //Write two functions that finds the factorial of any number.
+    //One should use recursive, the other should use a for loop.
+    //5! = 5*4*3*2*1
+
+    public static void main(String[] args) {
+        System.out.println(findFactorialRecursive(7));
+        System.out.println(findFactorialIterative(6));
+    }
+
+
+    private static int findFactorialRecursive(int number) {
+        if (number == 1 || number == 2) {
+            return number;
+        }
+
+        return number * findFactorialRecursive(number - 1);
+    }
+
+    private static int findFactorialIterative(int number) {
+        if (number == 1 || number == 2) {
+            return number;
+        }
+
+        int factorial = 1;
+        for (int i = number; i >= 2; i--) {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
